17b131c3b8c4f079d5a5cf0223f8bae2b589157f
Goryaeva Kseniya <goryaevakss@gmail.com>
2025-04-23 16:26:33 +0300

    add test file

--- tests/test_shop.py
+++ tests/test_shop.py
@@ -1,3 +1,23 @@
+import requests
+from selene import browser, have
+from urllib3 import request
 
+base_url = "https://aromacode.ru/"
+url_add_card = "https://aromacode.ru/product/attar-collection-sierra/"
+body = {
+    "sku_id": "87545",
+    "product_id": "25045"
+}
 
+url_cart = "https://aromacode.ru/cart/"
 
+def test_add_item_in_cart():
+    response = requests.post(url=url_add_card, data=body)
+    assert response.status_code == 200
+    cookies = response.cookies.get('landing')
+    print(response.cookies)
+
+    browser.open(base_url)
+    browser.driver.add_cookie({"name": "landing", "value": cookies})
+    browser.open(url_cart)
+    browser.element('.sku-code').should(have.text('арт.: 97545'))

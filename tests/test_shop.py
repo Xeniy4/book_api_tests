e52b3bff888535e4ed9cfcdcc2eae3d762e44c93
Goryaeva Kseniya <goryaevakss@gmail.com>
2025-04-30 21:08:59 +0300

    refactor

--- tests/test_shop.py
+++ tests/test_shop.py
@@ -1,3 +1,5 @@
+from wsgiref.validate import assert_
+
 import requests
 from selene import browser, have
 from urllib3 import request
@@ -21,3 +23,12 @@ def test_add_item_in_cart():
     # browser.driver.add_cookie({"name": "PHPSESSID", "value": cookies})
     # browser.open(url_cart)
     # browser.element('.sku-code').should(have.text('арт.: 97545'))
+
+
+def test_site():
+    response = requests.post(
+        url='https://www.holodilnik.ru/ajax/auth/login/',
+        data= {"login": "testbk01@bk.ru", "password": "123456789-"}
+    )
+    assert response.status_code == 200
+    print(response.text, response.status_code)
